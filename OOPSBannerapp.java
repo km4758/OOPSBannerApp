7085c9b57d6f4cb5dabfd9ada9674bc34eacfdb7
km4758 <km4758@srmist.edu.in>
2026-02-18 10:08:49 +0530

    Render OOPS as Banner using String Array and Loop

---
+++ OOPSBannerapp.java
@@ -0,0 +1,17 @@
+public class OOPSBannerapp {
+    public static void main(String[] args) {
+        String[] lines = new String[5];
+
+lines[0] = String.join("  ", "  ****  ", "  ****  ", " *****  ", "  ****  ");
+lines[1] = String.join("  ", " **  ** ", " **  ** ", " **  ** ", " **     ");
+lines[2] = String.join("  ", " **  ** ", " **  ** ", " *****  ", "  ****  ");
+lines[3] = String.join("  ", " **  ** ", " **  ** ", " **     ", "     ** ");
+lines[4] = String.join("  ", "  ****  ", "  ****  ", " **     ", "  ****  ");
+
+// Use a for-each loop to print each line
+for (String line : lines) {
+    System.out.println(line);
+}
+
+    }
+}
